b8b7035e158da9d667bac7233bf5fed3ac0d1088
caoming <johncming@yahoo.com>
2018-10-17 16:18:32 +0800

    tlmanager: add tests

--- tld_manager_test.go
+++ tld_manager_test.go
@@ -2,6 +2,7 @@ package tldwhois
 
 import (
 	"testing"
+	"time"
 )
 
 func Test_newTldManager(t *testing.T) {
@@ -11,5 +12,26 @@ func Test_newTldManager(t *testing.T) {
 	}
 	if got := int(m.load()); got != want {
 		t.Fatalf("want %d, got %d", want, got)
+	}
+}
+
+func TestStop(t *testing.T) {
+	m := newTldManager(tldsURL)
+
+	m.stop()
+
+	select {
+	case <-m.donec:
+	case <-time.After(time.Second * 5):
+		t.Fatal("stop too long")
+	}
+}
+
+func TestTimeout(t *testing.T) {
+	m := newTldManagerWithLimitAndTimeout(tldsURL, defaultLimit, time.Nanosecond)
+	select {
+	case <-m.donec:
+	case <-time.After(time.Second * 5):
+		t.Fatal("stop too long")
 	}
 }
